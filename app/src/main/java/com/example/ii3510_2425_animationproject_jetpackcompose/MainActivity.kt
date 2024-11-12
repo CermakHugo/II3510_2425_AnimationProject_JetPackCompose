84c22c1e74eaa104be524876cecf01d1018cee38
cerma <cermakhugo@mail.com>
2024-11-12 19:30:57 +0100

    add fade animation

--- app/src/main/java/com/example/ii3510_2425_animationproject_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/ii3510_2425_animationproject_jetpackcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
@@ -24,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +38,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             II3510_2425_AnimationProject_JetPackComposeTheme {
                 var visible: Boolean by remember { mutableStateOf(true) }
+                var fade: Boolean by remember { mutableStateOf(true) }
+                val animatedAlpha: Float by animateFloatAsState(
+                    targetValue = if (fade) 1f else 0f,
+                    label = "alpha"
+                )
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally,
@@ -59,8 +66,26 @@ class MainActivity : ComponentActivity() {
                             fontSize = 22.sp
                         )
                     }
+                    Box(
+                        modifier = Modifier
+                            .size(220.dp)
+                            .graphicsLayer{
+                                alpha = animatedAlpha
+                            }
+                            .background(Color(0XFF4A8FF3))
+
+                    )
+                    Spacer(modifier = Modifier.size(22.dp))
+                    Button(onClick = { fade = !fade }) {
+                        Text(
+                            text = if (fade) "Hide" else "Show",
+                            fontSize = 22.sp
+                        )
+                    }
                 }
+
             }
+
         }
     }
 }
